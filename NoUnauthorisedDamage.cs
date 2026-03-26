4333e82ad13c2ac5fb8efdd1c0be24e54d6ffbd4
Andrii Herheliuk <andrii@herheliuk.com>
2026-03-26 00:09:33 +0000

    1.1.4

---
+++ NoUnauthorisedDamage.cs
@@ -0,0 +1,29 @@
+namespace Oxide.Plugins;
+
+[Info("No Unauthorised Damage", "&anhe", "1.1.4")]
+[Description("Prevents players from damaging other players’ belongings.")]
+public class NoUnauthorisedDamage : RustPlugin
+{
+    private object OnEntityTakeDamage(BaseCombatEntity entity, HitInfo info) =>
+        (
+            // World
+            entity.OwnerID == 0 ||
+            // Player’s
+            (
+                info.InitiatorPlayer is { } player &&
+                (
+                    // Yours
+                    entity.OwnerID == player.userID ||
+                    // Team’s
+                    RelationshipManager.ServerInstance.FindTeam(player.currentTeam)
+                        ?.members.Contains(entity.OwnerID) == true ||
+                    // Authorised
+                    entity.GetBuildingPrivilege()
+                        ?.IsAuthed(player) == true ||
+                    // Admin
+                    player.IsAdmin
+                )
+            )
+        )
+            ? null : false;
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+Plugin that requires no configuration and works out of the box.
+
+Useful for PvE servers, as players can still damage windows and doors by default.
